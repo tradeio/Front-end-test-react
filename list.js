c824b9f0dd29d573fc4b8b23ad9b10424551b59d
petros-tio <48286330+petros-tio@users.noreply.github.com>
2022-04-13 11:20:53 +0300

    Add files via upload

---
+++ list.js
@@ -0,0 +1,27 @@
+const list = [
+	{
+		"instrument": "EUR/USD",
+		"price": "1.17567",
+		"sentiment": "bearish"
+	},
+	{
+		"instrument": "Gold",
+		"price": "1794.67",
+		"sentiment": "bullish"
+	},
+	{
+		"instrument": "GBP/USD",
+		"price": "1.35467",
+		"sentiment": "bearish"
+	},
+	{
+		"instrument": "USD/JPY",
+		"price": "109.745",
+		"sentiment": "mixed"
+	},
+	{
+		"instrument": "Wall Street",
+		"price": "35377.4",
+		"sentiment": "mixed"
+	}
+];
